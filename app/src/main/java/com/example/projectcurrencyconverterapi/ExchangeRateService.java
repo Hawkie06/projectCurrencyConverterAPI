8f6dd66cb619e8d7988835c2c6c8b752872e0e4c
Hawkie06 <gaviaonathally2006@gmail.com>
2025-08-22 11:54:56 -0300

    Implementa os métodos do retrofit

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,4 +1,11 @@
 package com.example.projectcurrencyconverterapi;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface ExchangeRateService {
+    /**
+     * Método paara obter taxas de câmbio
+     */
 }
